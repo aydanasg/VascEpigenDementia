be33922d2a36f05e5efd63e89d36743390585ef2
Aydan Asgarova <aydan.asgarova00@gmail.com>
2024-11-13 15:58:52 +0000

    Adding peaks, comitting from VS

--- scripts/DeepSTARR_preproc.R
+++ scripts/DeepSTARR_preproc.R
@@ -2,11 +2,15 @@ library(GenomicRanges)
 library(BSgenome.Hsapiens.UCSC.hg38)
 library(rtracklayer)
 
+#Loading the peak summits from a bed file and creating a 250bp region around them
+
 summits <- import(summitbed)
 start(summits) <- start(summits) - 125
 end(summits) <- end(summits) + 125
 
+#Loading the bam files
 bam <- import(bam)
+#Counting overlaps between the reads and the 250bp regions
 overlaps <- countOverlaps(summits, bam)
 
 seq <- BSgenome::getSeq(BSgenome.Hsapiens.UCSC.hg38,summits)
@@ -16,4 +20,5 @@ df <- cbind(chr, unlist(seq), overlaps)
 df <- data.frame(df)
 colnames(df) <- c("chr", "seq", "score")
 
+#Save to csv file to use in model training
 write.csv(df, outputPath, row.names = FALSE)
